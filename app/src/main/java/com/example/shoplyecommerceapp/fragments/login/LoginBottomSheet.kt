e168c0c20337a9e5386c87769a56999573566909
Tran Luong Tien Si <19522128@gm.uit.edu.vn>
2025-07-05 20:15:09 +0400

    update UI

---
+++ app/src/main/java/com/example/shoplyecommerceapp/fragments/login/LoginBottomSheet.kt
@@ -0,0 +1,36 @@
+package com.example.shoplyecommerceapp.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.shoplyecommerceapp.R
+import com.example.shoplyecommerceapp.databinding.FragmentTaskSheetBinding
+import com.example.shoplyecommerceapp.databinding.LoginBottomSheetBinding
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class LoginBottomSheet : BottomSheetDialogFragment() {
+    private var _binding: LoginBottomSheetBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = LoginBottomSheetBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.cancelLogin.setOnClickListener {
+            dismiss()
+        }
+    }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+//    override fun getTheme(): Int = R.style.BottomSheetDialogTheme
+}
